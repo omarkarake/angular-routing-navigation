b5cf4929c58bde844a847b298b22fa74e322f89e
omar karake <omar.karake@amalitechtraining.org>
2024-07-08 12:04:30 +0200

    configuring routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,39 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ProductComponent } from './product/product.component';
 import { BillingComponent } from './billing/billing.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SingleProductComponent } from './single-product/single-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     ProductComponent,
-    BillingComponent
+    BillingComponent,
+    NotFoundComponent,
+    SingleProductComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: 'product/:id', component: SingleProductComponent }, //should be uptop because it's more specific
+      { path: 'product', component: ProductComponent },
+      { path: 'billing', component: BillingComponent },
+      { path: '**', component: NotFoundComponent }, // ** this represents a wild card, if no url match
+    ]),
   ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
