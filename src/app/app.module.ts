ebee0ba9c00f084e5d4e0363ea55d8ea8309365d
omar karake <omar.karake@amalitechtraining.org>
2024-07-08 11:48:06 +0200

    I have three component, navigation-billing-product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProductComponent } from './product/product.component';
+import { BillingComponent } from './billing/billing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProductComponent,
+    BillingComponent
   ],
   imports: [
     BrowserModule,
